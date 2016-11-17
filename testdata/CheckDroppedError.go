637d0236d5cc295981a6ea49eef7975ff087a323
Frederick Akalin <akalin@gmail.com>
2016-11-16 21:42:11 -0800

    Add second test case

--- testdata/CheckDroppedError.go
+++ testdata/CheckDroppedError.go
@@ -16,3 +16,20 @@ func fn1() error {
 	}
 	return nil
 }
+
+func fn2() (int, error) {
+	var err error
+	if err != nil {
+		return 0, err
+	}
+	if err != nil {
+		if err.Error() == "foo" {
+			return 0, err
+		}
+		return 4, nil
+	}
+	if err != nil {
+		return 5, nil // MATCH /error dropped/
+	}
+	return 0, nil
+}
